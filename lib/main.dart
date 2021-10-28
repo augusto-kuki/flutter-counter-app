e05d4104f41cb3823fed37b3a6817ba9be12303d
Augusto Hirt Farias <augustokukimusic@gmail.com>
2021-10-28 16:55:52 -0300

    envio do repositorio

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Meu Primeiro App!")),
+      body: Center(
+        child: Text('Contador $count', textAlign: TextAlign.center),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
+}
